3a3d76b607fc252a8c9bbfa764fa6f630122bc65
konstgav <gavrilov_k@inbox.ru>
2022-03-19 16:13:57 +0000

    See https://github.com/difrateam/difra/commit/6cf9c9b7f5040fabfcfe00fc052201ffe8bbd65f from refs/tags/doc0.0.11

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'doc0.0.10',
+    VERSION: '0.0.1',
     LANGUAGE: 'ru',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
